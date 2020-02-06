6bc99bf9ea091e12f02a08b09c2e2f52a1cc2471
Pedro de Paula <pmdpaula@yahoo.com.br>
2020-02-06 15:51:24 -0300

    Carga inicial

---
+++ src/pessoa.js
@@ -0,0 +1,7 @@
+// import './modulos/moduloA'  // posso importar aqui ou em outro arquivo do projeto
+
+export default class Pessoa {
+  cumprimentar() {
+    return 'Bom dia!'
+  }
+}
---
+++ src/principal.js
@@ -0,0 +1,5 @@
+import Pessoa from './pessoa'
+// import './modulos/moduloA'  // posso importar aqui ou em outro arquivo do projeto
+
+const atendente = new Pessoa
+console.log(atendente.cumprimentar);
