194fd879a335ffa0de37707b14521f387f32935c
Pedro de Paula <pmdpaula@yahoo.com.br>
2020-02-07 23:30:22 -0300

    Fim do módulo de webpack

--- src/pessoa.js
+++ src/pessoa.js
@@ -1,4 +1,4 @@
-// import './modulos/moduloA'  // posso importar aqui ou em outro arquivo do projeto
+import './modulos/moduloA'  // posso importar aqui ou em outro arquivo do projeto
 
 export default class Pessoa {
   cumprimentar() {
--- src/principal.js
+++ src/principal.js
@@ -1,5 +1,6 @@
 import Pessoa from './pessoa'
-// import './modulos/moduloA'  // posso importar aqui ou em outro arquivo do projeto
+// import './modulos/moduloA'   // posso importar aqui ou em outro arquivo do projeto. Foi posto no pessoa.js
+import './assets'               // procura arquivo index.js no diretório indicado.
 
 const atendente = new Pessoa
-console.log(atendente.cumprimentar);
+console.log(atendente.cumprimentar());
